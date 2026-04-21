c2ba43333544551277295013d8885de1366082f5
TonyHuang <hzx0109@gmail.com>
2026-04-21 16:47:36 -0700

    update projects page with new projects

--- projects.php
+++ projects.php
@@ -108,7 +108,7 @@ include 'inc/header.php';
                         </div>
 
                         <!-- Block 2 -->
-                        <div class="projects-block" data-country="south-africa" data-type="residential">
+                        <div class="projects-block" data-country="australia" data-type="residential">
                             <div class="row mb-4">
                                 <div class="col-lg-10 offset-lg-2">
                                     <h2 class="mb-4">Ballara House</h2>
@@ -130,7 +130,7 @@ include 'inc/header.php';
                         </div>
 
                         <!-- Block 3 -->
-                        <div class="projects-block" data-country="china" data-type="amenities">
+                        <div class="projects-block" data-country="australia" data-type="commercial">
                             <div class="row mb-5 pb-5">
                                 <div class="col-lg-5 projects-feature-media-col">
                                     <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/main2.jpeg" alt="Project 1" class="projects-feature-media-img">
@@ -174,6 +174,274 @@ include 'inc/header.php';
 
                                 <div class="col-lg-5 offset-lg-1">
                                     <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/main4.png" alt="Project 3" class="img-fluid w-100">
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Block 5 -->
+                        <div class="projects-block" data-country="australia" data-type="commercial">
+                            <div class="row mb-5 pb-5">
+                                <div class="col-lg-7">
+                                    <h2 class="mb-4">Site Office</h2>
+                                    <p class="text-uppercase mb-5">Modular Office Facility</p>
+                                    <div class="row">
+                                        <div class="col-lg-5 offset-lg-5">
+                                            <p>A rapidly deployed modular workspace designed to support on-site operations.</p>
+                                            <a href="#" class="projects-more-details-link">MORE DETAILS</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-5 projects-feature-media-col">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/main2.jpeg" alt="Site Office" class="projects-feature-media-img">
+                                </div>
+                            </div>
+
+                            <div class="row bottom-padding-lg projects-content-overlap-row">
+                                <div class="col-lg-7 offset-lg-3">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/slide2.jpeg" alt="Site Office" class="img-fluid w-100">
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Block 6 -->
+                        <div class="projects-block" data-country="australia" data-type="amenities">
+                            <div class="row mb-4">
+                                <div class="col-lg-10 offset-lg-2">
+                                    <h2 class="mb-4">Cafeteria Facility</h2>
+                                    <p class="text-uppercase mb-5">Modular Dining Facility</p>
+                                </div>
+                            </div>
+
+                            <div class="row bottom-padding-lg">
+                                <div class="col-lg-5">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/main4.png" alt="Cafeteria Facility" class="img-fluid w-100">
+                                </div>
+                                <div class="col-lg-5 offset-lg-1 d-flex flex-column justify-content-center">
+                                    <div>
+                                        <p class="mb-4">A functional modular structure providing dining and communal space for workforce environments.</p>
+                                        <a href="#" class="projects-more-details-link">MORE DETAILS</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Block 7 -->
+                        <div class="projects-block" data-country="australia" data-type="amenities">
+                            <div class="row mb-5 pb-5">
+                                <div class="col-lg-5 projects-feature-media-col">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/main2.jpeg" alt="Canteen Facility" class="projects-feature-media-img">
+                                </div>
+
+                                <div class="col-lg-7">
+                                    <h2 class="mb-4">Canteen Facility</h2>
+                                    <p class="text-uppercase mb-5">Modular Food Service Unit</p>
+                                    <div class="row">
+                                        <div class="col-lg-5 offset-lg-5">
+                                            <p>Designed for efficient food service operations within remote or temporary sites.</p>
+                                            <a href="#" class="projects-more-details-link">MORE DETAILS</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row bottom-padding-lg projects-content-overlap-row">
+                                <div class="col-lg-7 offset-lg-3">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/slide2.jpeg" alt="Canteen Facility" class="img-fluid w-100">
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Block 8 -->
+                        <div class="projects-block" data-country="australia" data-type="amenities">
+                            <div class="row mb-4">
+                                <div class="col-lg-10 offset-lg-2">
+                                    <h2 class="mb-4">Portable Washrooms</h2>
+                                    <p class="text-uppercase mb-5">Sanitation Units</p>
+                                </div>
+                            </div>
+
+                            <div class="row bottom-padding-lg">
+                                <div class="col-lg-5 d-flex flex-column justify-content-center">
+                                    <div>
+                                        <p class="mb-4">Prefabricated modular washroom facilities designed for hygiene, durability, and ease of deployment.</p>
+                                        <a href="#" class="projects-more-details-link">MORE DETAILS</a>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-5 offset-lg-1">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/main4.png" alt="Portable Washrooms" class="img-fluid w-100">
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Block 9 -->
+                        <div class="projects-block" data-country="australia" data-type="amenities">
+                            <div class="row mb-5 pb-5">
+                                <div class="col-lg-7">
+                                    <h2 class="mb-4">Laundry Facilities</h2>
+                                    <p class="text-uppercase mb-5">Modular Utility Units</p>
+                                    <div class="row">
+                                        <div class="col-lg-5 offset-lg-5">
+                                            <p>Supporting infrastructure units designed for operational convenience in workforce environments.</p>
+                                            <a href="#" class="projects-more-details-link">MORE DETAILS</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-5 projects-feature-media-col">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/main2.jpeg" alt="Laundry Facilities" class="projects-feature-media-img">
+                                </div>
+                            </div>
+
+                            <div class="row bottom-padding-lg projects-content-overlap-row">
+                                <div class="col-lg-7 offset-lg-3">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/slide2.jpeg" alt="Laundry Facilities" class="img-fluid w-100">
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Block 10 -->
+                        <div class="projects-block" data-country="australia" data-type="commercial">
+                            <div class="row mb-4">
+                                <div class="col-lg-10 offset-lg-2">
+                                    <h2 class="mb-4">Warwick Farm Structure</h2>
+                                    <p class="text-uppercase mb-5">Agricultural Modular Facility</p>
+                                </div>
+                            </div>
+
+                            <div class="row bottom-padding-lg">
+                                <div class="col-lg-5">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/main4.png" alt="Warwick Farm Structure" class="img-fluid w-100">
+                                </div>
+                                <div class="col-lg-5 offset-lg-1 d-flex flex-column justify-content-center">
+                                    <div>
+                                        <p class="mb-4">A modular building solution applied within a farm setting, demonstrating system adaptability beyond residential and commercial use.</p>
+                                        <a href="#" class="projects-more-details-link">MORE DETAILS</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Block 11 -->
+                        <div class="projects-block" data-country="china" data-type="residential">
+                            <div class="row mb-5 pb-5">
+                                <div class="col-lg-5 projects-feature-media-col">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/main2.jpeg" alt="Zambia House" class="projects-feature-media-img">
+                                </div>
+
+                                <div class="col-lg-7">
+                                    <h2 class="mb-4">Zambia House</h2>
+                                    <p class="text-uppercase mb-5">2-Bedroom Residential Home</p>
+                                    <div class="row">
+                                        <div class="col-lg-5 offset-lg-5">
+                                            <p>A modular home built using Luxwood panels, combining structural performance with efficient construction methods.</p>
+                                            <a href="#" class="projects-more-details-link">MORE DETAILS</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row bottom-padding-lg projects-content-overlap-row">
+                                <div class="col-lg-7 offset-lg-3">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/slide2.jpeg" alt="Zambia House" class="img-fluid w-100">
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Block 12 -->
+                        <div class="projects-block" data-country="china" data-type="residential">
+                            <div class="row mb-4">
+                                <div class="col-lg-10 offset-lg-2">
+                                    <h2 class="mb-4">Beijing House</h2>
+                                    <p class="text-uppercase mb-5">1-Bedroom Residential Home</p>
+                                </div>
+                            </div>
+
+                            <div class="row bottom-padding-lg">
+                                <div class="col-lg-5 d-flex flex-column justify-content-center">
+                                    <div>
+                                        <p class="mb-4">A compact modular housing solution demonstrating adaptability within urban environments.</p>
+                                        <a href="#" class="projects-more-details-link">MORE DETAILS</a>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-5 offset-lg-1">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/main4.png" alt="Beijing House" class="img-fluid w-100">
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Block 13 -->
+                        <div class="projects-block" data-country="south-africa" data-type="commercial">
+                            <div class="row mb-5 pb-5">
+                                <div class="col-lg-7">
+                                    <h2 class="mb-4">Theodor Herzl School</h2>
+                                    <p class="text-uppercase mb-5">Educational Facility | 100+ Students</p>
+                                    <div class="row">
+                                        <div class="col-lg-5 offset-lg-5">
+                                            <p>A modular school development designed to accommodate over 100 students, showcasing the scalability and reliability of the Luxwood system in community infrastructure.</p>
+                                            <a href="#" class="projects-more-details-link">MORE DETAILS</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-5 projects-feature-media-col">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/main2.jpeg" alt="Theodor Herzl School" class="projects-feature-media-img">
+                                </div>
+                            </div>
+
+                            <div class="row bottom-padding-lg projects-content-overlap-row">
+                                <div class="col-lg-7 offset-lg-3">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/slide2.jpeg" alt="Theodor Herzl School" class="img-fluid w-100">
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Block 14 -->
+                        <div class="projects-block" data-country="south-africa" data-type="residential">
+                            <div class="row mb-4">
+                                <div class="col-lg-10 offset-lg-2">
+                                    <h2 class="mb-4">Cornubia House</h2>
+                                    <p class="text-uppercase mb-5">2-Level | 3-Bedroom Residential Home</p>
+                                </div>
+                            </div>
+
+                            <div class="row bottom-padding-lg">
+                                <div class="col-lg-5">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/main4.png" alt="Cornubia House" class="img-fluid w-100">
+                                </div>
+                                <div class="col-lg-5 offset-lg-1 d-flex flex-column justify-content-center">
+                                    <div>
+                                        <p class="mb-4">A multi-level residential project demonstrating structural strength and architectural flexibility.</p>
+                                        <a href="#" class="projects-more-details-link">MORE DETAILS</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Block 15 -->
+                        <div class="projects-block" data-country="south-africa" data-type="residential">
+                            <div class="row mb-5 pb-5">
+                                <div class="col-lg-5 projects-feature-media-col">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/main2.jpeg" alt="Brittania Bay Residence" class="projects-feature-media-img">
+                                </div>
+
+                                <div class="col-lg-7">
+                                    <h2 class="mb-4">Brittania Bay Residence</h2>
+                                    <p class="text-uppercase mb-5">2-Level | 2-Bedroom Residential Home</p>
+                                    <div class="row">
+                                        <div class="col-lg-5 offset-lg-5">
+                                            <p>A coastal residential development designed to perform in demanding environmental conditions.</p>
+                                            <a href="#" class="projects-more-details-link">MORE DETAILS</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row bottom-padding-lg projects-content-overlap-row">
+                                <div class="col-lg-7 offset-lg-3">
+                                    <img src="https://tref.digitaldesignnyc.co/wp-content/uploads/2023/12/slide2.jpeg" alt="Brittania Bay Residence" class="img-fluid w-100">
                                 </div>
                             </div>
                         </div>
